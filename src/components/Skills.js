bad951baed2ef8313f2a9b2f2adfd2d92b939aac
Anurag Banerjee <anurag.banerjee011@gmail.com>
2023-02-16 00:55:15 +0530

    added icons for skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,32 +1,70 @@
 import React from 'react';
 import "../styles/Skills.css";
 
+
+const tooltip_helper = text => (
+  `${text} (click for more info)`
+)
+
 function Skills() {
   return (
     <>
       {/* skills section */}
       <div className="skills">
-        <h1> Skills</h1>
-        <ol className="list">
-          <li className="item">
-            <h3> Front-End</h3>
-            <span>
-              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
-            </span>
-          </li>
-          <li className="item">
-            <h3>Back-End</h3>
-            <span>
-              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
-            </span>
-          </li>
-          <li className="item">
-            <h3>Languages</h3>
-            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
-          </li>
-        </ol>
+
+        {/* <h1>Skills</h1> */}
+        <div className='container'>
+
+          {/* container left */}
+          <div className='top-expertise-left'>
+            <h4>Top Expertise</h4>
+            <div className="icon-holder">
+
+              <span className='java logo tooltip'>
+                <span className='tooltiptext'>{tooltip_helper("Java NPTEL")}</span>
+              </span>
+
+              <span className='spring logo tooltip rounded'>
+                <span className='tooltiptext'>{tooltip_helper("SpringBoot")}</span>
+              </span>
+
+              <span className='react logo tooltip rounded'>
+                <span className='tooltiptext'>{tooltip_helper("React-Redux")}</span>
+              </span>
+
+              <span className='aws-clfc01 logo tooltip rounded'>
+                <span class="tooltiptext">{tooltip_helper("AWS Certified Cloud Practationer clf-c01")}</span>
+              </span>
+
+              <span className='docker logo tooltip'>
+                <span class="tooltiptext">{tooltip_helper("Docker")}</span>
+              </span>
+
+              <span className='ansible logo tooltip'>
+                <span class="tooltiptext">{tooltip_helper("Ansible")}</span>
+              </span>
+
+              <span className='terraform logo tooltip'>
+                <span class="tooltiptext">{tooltip_helper("TerraForm")}</span>
+              </span>
+
+              <span className='python logo tooltip'>
+                <span class="tooltiptext">{tooltip_helper("Python")}</span>
+              </span>
+
+              <span className='javascript logo tooltip'>
+                <span class="tooltiptext">{tooltip_helper("Javascript")}</span>
+              </span>
+
+            </div>
+          </div>
+
+          {/* container right */}
+          <div className='more-skills-right'>
+            <h4>More Skills</h4>
+          </div>
+
+        </div>
       </div>
     </>
   );
