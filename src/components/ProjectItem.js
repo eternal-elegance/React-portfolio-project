19d8f7cb636168bd24df6f71c4276942080b18ef
Anurag Banerjee <anurag.banerjee011@gmail.com>
2023-02-17 13:05:11 +0530

    added new transition to project page

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -4,14 +4,26 @@ import { useNavigate } from "react-router-dom";
 function ProjectItem({ image, name, id }) {
   const navigate = useNavigate();
   return (
-    <div
-      className="projectItem"
-      onClick={() => {
-        navigate("/project/" + id);
-      }}
-    >
-      <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
-      <h1> {name} </h1>
+    // <div
+    //   className="projectItem"
+    //   onClick={() => {
+    //     navigate("/project/" + id);
+    //   }}
+    // >
+    //   <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
+    //   <h1> {name} </h1>
+    // </div>
+
+    <div className="card">
+      {/* image div */}
+      <div className="image">
+        <img href="#" src={image} />
+      </div>
+
+      <div className="content">
+        <h3>{name}</h3>
+        <p>DIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
+      </div>
     </div>
   );
 }
