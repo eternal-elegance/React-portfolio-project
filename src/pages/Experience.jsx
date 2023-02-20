0813b873ea71f7658c36c9a1d15f59f1354b3531
Anurag Banerjee <anurag.banerjee011@gmail.com>
2023-02-20 23:33:20 +0530

    completed basic project structure.
    add more in future (weekends probably)

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,53 +6,69 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@material-ui/icons/School";
 import WorkIcon from "@material-ui/icons/Work";
+import '../styles/Experience.css'
 
 function Experience() {
   return (
     <div className="experience">
       <VerticalTimeline lineColor="#3e497a">
+
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2010 - 2014"
+          date="2015 - 2016"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            My Random High School, Random Place, Random State
+            Rajendra Vidyalaya, Jamshedpur, Jharkhand
           </h3>
-          <p> High School Diploma</p>
+          <p> ICSE</p>
         </VerticalTimelineElement>
+
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2014 - 2018"
+          date="2016 - 2018"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            My Cool University, Vancouver, British Columbia
+            Rajendra Vidyalaya, Jamshedpur, Jharkhand
+          </h3>
+          <p> ISC</p>
+        </VerticalTimelineElement>
+
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018 - 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Bengal Institute of Technology <i>(A unit of Techno India Group)</i>, Kolkata, West Bengal
           </h3>
 
           <h4 className="vertical-timeline-element-subtitle">
-            Bachelor's Degree
+            <strong>Bachelor of Technology</strong>
           </h4>
 
-          <p> Computer Science</p>
+          <p> Information Technology</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2018 - 2020"
+          date="2022 - present"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Back End Engineer - Google
+            Senior Software Engineer at Capgemini Technology Services India Limited
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
+            Chennai, IN
           </h4>
-          <p>Developed the backend infrastructure for 3 projects.</p>
+          <p>Started my career as a Java EE Fullstack with React developer with knowledge of devops and IT concepts.</p>
         </VerticalTimelineElement>
-
+        {/* 
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2020 - present"
@@ -69,7 +85,7 @@ function Experience() {
             Helped the team launch 2 major features by working both in the front
             end and back end.
           </p>
-        </VerticalTimelineElement>
+        </VerticalTimelineElement> */}
       </VerticalTimeline>
     </div>
   );
